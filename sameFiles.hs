fe51109e8672672f15f40a407866496b1f39e58b
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2014-07-02 23:51:05 -0700

    Added cost centre to hash. Turns out it takes about 50% of execution time.

--- sameFiles.hs
+++ sameFiles.hs
@@ -1,7 +1,7 @@
 import Pipes (yield, lift, (~>))
 import Pipes.Prelude (fold)
 import System.Environment (getArgs)
-import Crypto.Hash.SHA1 
+import Crypto.Hash.SHA1 (hash)
 import qualified Data.ByteString as B
 import qualified Data.HashMap.Strict as H
 import qualified Data.Traversable as T
@@ -11,7 +11,7 @@ gethashmap :: FilePath -> IO (H.HashMap B.ByteString [FilePath])
 gethashmap = fold (\hashmap (sha1,file) -> H.insertWith (++) sha1 [file] hashmap)  H.empty id . getHashes where
     getHashes = getRecursiveContents ~> \file -> do
         contents <- lift $ B.readFile file
-        yield (hash contents, file)
+        {-# SCC "hash" #-} yield (hash contents, file)
 
 main :: IO ()
 main = do
