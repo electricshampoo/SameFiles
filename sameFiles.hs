a65d7cf1c9f121fb32d8ead9390080fcd8e4c32a
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2014-07-20 22:02:26 -0700

    About 2x faster with prefix checking. The size of the prefix check is also user tunable.

--- sameFiles.hs
+++ sameFiles.hs
@@ -1,24 +1,49 @@
 import Pipes (yield, liftIO, (~>))
 import Pipes.Prelude (fold)
+import Data.List (group, sort)
 import System.Environment (getArgs)
 import Crypto.Hash.SHA1 (hash)
 import Control.Monad (when)
-import qualified Data.ByteString as B (ByteString, readFile)
+import System.IO (openFile, IOMode(ReadMode))
+import qualified Data.ByteString as B (ByteString, readFile, hGetSome, readFile)
 import qualified Data.HashMap.Strict as H (HashMap, insertWith, empty)
-import qualified Data.Foldable as F (mapM_)
+import qualified Data.Foldable as F (forM_)
 import Util.StreamDirectory (getRecursiveContents)
 
---you can use file contents directly without sha1 but this
---requires you to keep the whole contents of the file in memory
---for each file
-similarFiles :: FilePath -> IO (H.HashMap B.ByteString [FilePath])
-similarFiles = fold (\hashmap (sha1,file) -> H.insertWith (++) sha1 [file] hashmap)  H.empty id . getHashes where
-    getHashes = getRecursiveContents ~> \file -> do
-        contents <- liftIO $ B.readFile file
-        yield (hash contents, file)
+possiblySimilarFiles :: Int -> FilePath -> IO (H.HashMap B.ByteString [FilePath])
+possiblySimilarFiles n = fold (\hashmap (chunk, file) -> H.insertWith (++) chunk [file] hashmap)  H.empty id . getChunks where
+    getChunks = getRecursiveContents ~> \file -> do
+        chunck <- liftIO $ getNBytes n file
+        yield (chunck, file)
+
+getNBytes :: Int -> FilePath -> IO B.ByteString
+getNBytes n file = do
+    h <- openFile file ReadMode 
+    B.hGetSome h n
+
+data HashPair = Pair !B.ByteString !FilePath
+
+instance Eq HashPair where
+    (Pair h1 _) == (Pair h2 _) = h1 == h2
+
+instance Ord HashPair where
+    compare (Pair h1 _) (Pair h2 _) = compare h1 h2
+
+instance Show HashPair where
+    show (Pair _ filename) = filename
+
+getHash :: FilePath -> IO HashPair
+getHash file = do 
+    contents <- B.readFile file
+    return $! Pair (hash contents) file
 
 main :: IO ()
 main = do
-    [dir] <- getArgs
-    hashmap <- similarFiles dir
-    F.mapM_ (\x -> when (length x > 1) (print x)) hashmap
+    [dir, num] <- getArgs
+    hashmap <- possiblySimilarFiles (read num) dir
+    F.forM_ hashmap $ \x -> do
+        when (sufficientlyLarge x) $ do
+            pairs <- mapM getHash x
+            mapM_ (\y -> when (sufficientlyLarge y) (print y)) . group . sort $ pairs
+    where sufficientlyLarge [_] = False --lists are never empty so avoid checking for them
+          sufficientlyLarge _ = True
