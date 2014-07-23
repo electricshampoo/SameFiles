8cd41a2990bc34c8e355b6ca590bf540b51efa60
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2014-07-23 13:05:46 -0700

    Added additional check for the first 64 bytes of file and refactored a little more.

--- sameFiles.hs
+++ sameFiles.hs
@@ -4,34 +4,41 @@ import System.Environment (getArgs)
 import Crypto.Hash.MD5 (hash)
 import Control.Monad (when)
 import Data.Map.Strict (Map, empty, alter)
-import Data.Foldable (forM_)
-import qualified Data.ByteString as B (ByteString, readFile)
+import Data.Foldable (forM_, foldr', foldrM)
+import System.IO (withFile, IOMode(ReadMode))
+import qualified Data.ByteString as B (ByteString, readFile, hGetSome)
 import Util.StreamDirectory (getRecursiveContents)
 import System.Posix (getFileStatus, fileSize, FileOffset)
 
-possiblySimilarFiles ::  FilePath -> IO (Map FileOffset [FilePath])
-possiblySimilarFiles dir = foldM add (return empty) return $ getRecursiveContents dir where
-    add intmap file = do
+sameSizeFiles ::  FilePath -> IO (Map FileOffset [FilePath])
+sameSizeFiles = foldM add (return empty) return . getRecursiveContents where
+    add collisionMap file = do
         size <- fmap fileSize $ getFileStatus file
-        let f Nothing = Just $! [file]
-            f (Just list) = Just $! file:list
-        return $! alter f size intmap
+        return $! alter (Just . maybe [file] (file:)) size collisionMap
 
-data HashPair = Pair {-# UNPACK #-} !B.ByteString !FilePath
+checkBytes :: Map a [FilePath] -> IO (Map B.ByteString [FilePath])
+checkBytes = foldrM add empty where
+    add collision collisionMap = do
+        pairs <- flip mapM collision $ \file -> do
+            prefix <- withFile file ReadMode (flip B.hGetSome 64)
+            return $! Pair prefix file
+        return $! foldr' (\(Pair prefix file) cmap -> alter (Just . maybe [file] (file:)) prefix cmap) collisionMap pairs
+        
+data Pair = Pair {-# UNPACK #-} !B.ByteString !FilePath
 
-instance Eq HashPair where
+instance Eq Pair where
     (Pair h1 _) == (Pair h2 _) = h1 == h2
 
-instance Ord HashPair where
+instance Ord Pair where
     compare (Pair h1 _) (Pair h2 _) = compare h1 h2
 
-instance Show HashPair where
+instance Show Pair where
     show (Pair _ filename) = filename
 
 main :: IO ()
 main = do
     [dir] <- getArgs
-    collisionMap <- possiblySimilarFiles dir
+    collisionMap <- sameSizeFiles dir >>= checkBytes
     forM_ collisionMap $ \collision -> do
         when (sufficientlyLarge collision) $ do
             pairs <- mapM getHash collision
